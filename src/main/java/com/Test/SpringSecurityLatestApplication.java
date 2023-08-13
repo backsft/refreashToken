1cb9f6e39e02aad94b1f7f35294ec19118d4c6ef
Sumon <Sumon@192.168.2.12>
2023-08-13 15:41:19 +0600

    sta

--- src/main/java/com/Test/SpringSecurityLatestApplication.java
+++ src/main/java/com/Test/SpringSecurityLatestApplication.java
@@ -8,7 +8,7 @@ public class SpringSecurityLatestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityLatestApplication.class, args);
-		System.out.println("Refreash token program started");
+		System.out.println("Refreash token program started test");
 	}
 
 }
